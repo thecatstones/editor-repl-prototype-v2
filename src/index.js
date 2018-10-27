06fb1af45d11b77c2d058e50a5893679a112748a
Nick Johnson <njohnson7@users.noreply.github.com>
2018-10-27 01:13:23 -0700

    added webpack-dev-server for auto-reload
    Co-authored-by: YingCGooi <25574844+YingCGooi@users.noreply.github.com>
    Co-authored-by: Julius <Julius@Juliuss-MacBook-Pro.local>
    Co-authored-by: Nick Johnson <njohnson7@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import _ from 'lodash'
+import './style.css'
 
 function component() {
   let element = document.createElement('div')
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
+  console.log(42)
   return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
+  // devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
   },
 }
